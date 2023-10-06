b367bde3ae6343d6a126a1160e4dd12c6ccc19d5
ehuntober <ehuntercrypto@gmail.com>
2023-10-06 12:38:41 +0000

    new update

--- script.js
+++ script.js
@@ -7,36 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-
-const typingText = document.getElementById('typing-text');
-const originalText = "Are you a student ";
-
-function typeText() {
-  typingText.textContent = '';
-  let i = 0;
-
-  function type() {
-    if (i < originalText.length) {
-      typingText.textContent += originalText.charAt(i);
-      i++;
-      setTimeout(type, 100); // Adjust typing speed (milliseconds)
-    } else {
-      setTimeout(eraseText, 2000); // Wait for 2 seconds before erasing
-    }
-  }
-
-  function eraseText() {
-    if (i >= 0) {
-      typingText.textContent = originalText.substring(0, i);
-      i--;
-      setTimeout(eraseText, 50); // Adjust erasing speed (milliseconds)
-    } else {
-      setTimeout(type, 1000); // Wait for 1 second before typing again
-    }
-  }
-
-  type();
-}
-
-// Start typing on page load
-typeText();
