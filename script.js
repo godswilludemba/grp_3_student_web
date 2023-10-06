6167dd80f9ad22293b54c8fdfff2bcbfba72d4b0
ehuntober <ehuntercrypto@gmail.com>
2023-10-06 10:01:27 +0000

    created a sign up for the  studentstackachieve

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate a delay of 8 seconds (8000 milliseconds)
+  setTimeout(function () {
+    // Hide preloader when content is loaded
+    document.body.classList.add("loaded");
+  }, 2000);
+});
 
-let menuOpen = false;
 
-function toggleMenu() {
-  const navLinks = document.querySelector('.nav-links');
-  const menuIcon = document.querySelector('.menu-icon');
 
-  if (menuOpen) {
-    navLinks.style.display = 'none';
-    menuIcon.textContent = '☰';
-  } else {
-    navLinks.style.display = 'block';
-    menuIcon.textContent = '✕';
+const typingText = document.getElementById('typing-text');
+const originalText = "Are you a student ";
+
+function typeText() {
+  typingText.textContent = '';
+  let i = 0;
+
+  function type() {
+    if (i < originalText.length) {
+      typingText.textContent += originalText.charAt(i);
+      i++;
+      setTimeout(type, 100); // Adjust typing speed (milliseconds)
+    } else {
+      setTimeout(eraseText, 2000); // Wait for 2 seconds before erasing
+    }
   }
 
-  menuOpen = !menuOpen;
+  function eraseText() {
+    if (i >= 0) {
+      typingText.textContent = originalText.substring(0, i);
+      i--;
+      setTimeout(eraseText, 50); // Adjust erasing speed (milliseconds)
+    } else {
+      setTimeout(type, 1000); // Wait for 1 second before typing again
+    }
+  }
+
+  type();
 }
 
-
+// Start typing on page load
+typeText();
